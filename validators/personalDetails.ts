49aa22b7c326db3cf24f17aeea80bd41361193cb
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-01 22:20:46 +0530

    updated validator

--- validators/personalDetails.ts
+++ validators/personalDetails.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const personalDetailsSchema = z.object({
   name: z.string().trim().min(2, "Name must be at least 2 characters"),
-  email: z.string().trim().email("Enter a valid email"),
   phone: z
     .string()
     .trim()
