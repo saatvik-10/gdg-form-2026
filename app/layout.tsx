91dccfd9d36f115392b2e594294ee96a7d85d587
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-01 22:22:12 +0530

    wrapped with a provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,30 @@
-import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { FormProvider } from "@/context/FormContext";
+import type { Metadata, Viewport } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Providers from '@/components/providers';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "GDG MIT-WPU Recruitment Form",
+  title: 'GDG MIT-WPU Recruitment Form',
   description:
-    "Apply to join GDG on Campus at MIT-WPU. Fill out the recruitment form to embark on your journey.",
+    'Apply to join GDG on Campus at MIT-WPU. Fill out the recruitment form to embark on your journey.',
 };
 
 export const viewport: Viewport = {
-  width: "device-width",
+  width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-  themeColor: "#0f172a",
+  themeColor: '#0f172a',
 };
 
 export default function RootLayout({
@@ -33,11 +33,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang='en' className='h-full antialiased'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-full flex flex-col bg-slate-950`}
       >
-        <FormProvider>{children}</FormProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
