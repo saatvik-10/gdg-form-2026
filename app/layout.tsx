bf212040bf486d026293be0908a33256614fc657
saatvik-10 <saatvikmadan529@gmail.com>
2026-09-01 22:27:17 +0530

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export const metadata: Metadata = {
   title: 'GDG MIT-WPU Recruitment Form',
   description:
     'Apply to join GDG on Campus at MIT-WPU. Fill out the recruitment form to embark on your journey.',
+  icons: { icon: '/assets/gdg-logo.png' },
 };
 
 export const viewport: Viewport = {
